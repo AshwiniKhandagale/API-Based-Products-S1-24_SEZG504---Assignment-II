89e1164747f751ed2a2510bf0ce687b89ccf07b9
Shraddha-2023mt93232 <167780730+Shraddha-2023mt93232@users.noreply.github.com>
2024-11-11 18:59:46 +0530

    Update app.js
    added code for CORS

--- food_delivery_system/app.js
+++ food_delivery_system/app.js
@@ -8,10 +8,12 @@ const deliveryRoutes = require('./routes/deliveryRoutes');
 const adminRoutes = require('./routes/adminRoutes');
 const errorHandler = require('./middlewares/errorHandler');
 const { swaggerUi, specs } = require('./swagger');
+const cors = require("cors");
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 mongoose.connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('Database connected'))
